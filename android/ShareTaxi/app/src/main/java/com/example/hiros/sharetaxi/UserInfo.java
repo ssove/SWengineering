007b1c0360fb9aab36685eaa8d48ab837a2866b3
Hiros-PC\Hiros <ja00800@nate.com>
2018-04-24 23:35:23 +0900

    init singleton Userinfo

---
+++ android/ShareTaxi/app/src/main/java/com/example/hiros/sharetaxi/UserInfo.java
@@ -0,0 +1,31 @@
+package com.example.hiros.sharetaxi;
+
+/**
+ * Created by Hiros on 2018-04-24.
+ */
+
+public class UserInfo {
+
+    private volatile static UserInfo instance;
+
+    public double y;
+    public double x;
+    public String nknm;
+
+    public static UserInfo getInstance() {
+        if(instance == null) {
+            synchronized (sgtSocket.class) {
+                if (instance == null) {
+                    instance = new UserInfo();
+                }
+            }
+        }
+        return instance;
+    }
+
+    private UserInfo() {
+        y = 0;
+        x = 0;
+        nknm = "TEMPUSER";
+    }
+}
